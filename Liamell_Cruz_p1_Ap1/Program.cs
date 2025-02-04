db902584b7128a9df0ee7700be88df1dc4ca729e
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-02-03 20:08:40 -0500

    Inyeccion del contexto en el program

--- Liamell_Cruz_p1_Ap1/Program.cs
+++ Liamell_Cruz_p1_Ap1/Program.cs
@@ -1,10 +1,18 @@
 using Liamell_Cruz_p1_Ap1.Components;
+using Liamell_Cruz_p1_Ap1.DAL;
+using Liamell_Cruz_p1_Ap1.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
+
+builder.Services.AddScoped<Ejemplo>();
 
 var app = builder.Build();
 
