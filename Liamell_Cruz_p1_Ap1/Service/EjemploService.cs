9b19e2ab0c95528e146a3e0b8dd172812df99b12
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-02-03 20:08:15 -0500

    Service

---
+++ Liamell_Cruz_p1_Ap1/Service/EjemploService.cs
@@ -0,0 +1,56 @@
+﻿using Liamell_Cruz_p1_Ap1.DAL;
+using Liamell_Cruz_p1_Ap1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Liamell_Cruz_p1_Ap1.Service;
+
+public class EjemploService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<bool> Guardar()
+    {
+        return true;
+       
+        
+    }
+
+    private async Task<bool> Existe()
+    {
+        return true;
+        
+
+
+    }
+
+    private async Task<bool> Insertar()
+    {
+        return true;
+
+    }
+
+    private async Task<bool> Modificar()
+    {
+        return true;
+
+    }
+
+    public async Task<bool> Eliminar()
+    {
+        return true;
+
+
+    }
+
+    public async Task<bool> Buscar()
+    {
+        return true;
+
+    }
+
+    public async Task<List<Ejemplo>> Listar(Expression<Func<Ejemplo, bool>> criterio)
+    {
+        
+
+
+    }
+}
