cc00db45a37020613d66658bbb0b745091e2758e
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-02-03 20:07:50 -0500

    Contexto

---
+++ Liamell_Cruz_p1_Ap1/DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Liamell_Cruz_p1_Ap1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Liamell_Cruz_p1_Ap1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+    }
+
+    public DbSet<Ejemplo> Ejemplo{ get; set; }   
+
+}
