099afaca29120b9fb427787f62e7cdfe21a3267d
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-02-17 19:35:03 -0500

    Modelo Aportes

---
+++ Liamell_Cruz_p1_Ap1/Models/Aportes.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Liamell_Cruz_p1_Ap1.Models;
+
+public class Aportes
+
+{
+    [Key]
+
+    public int AporteId { get; set; }
+
+    [Required(ErrorMessage ="Campo Obligatorio")]
+    public DateOnly Fecha{ get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    [RegularExpression(@"^[a-zA-z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string Persona { get; set; }
+
+    [RegularExpression(@"^[a-zA-z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string Observacion { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public double Monto { get; set; }
+
+
+
+
+}
