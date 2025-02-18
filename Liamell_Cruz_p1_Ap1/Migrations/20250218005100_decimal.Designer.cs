c42db0dc7bd5ab2637f81a061d00f16cc68c5b18
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-02-17 20:03:02 -0500

    Cambio de double a decimal

--- Liamell_Cruz_p1_Ap1/Migrations/20250218005100_decimal.Designer.cs
+++ Liamell_Cruz_p1_Ap1/Migrations/20250218005100_decimal.Designer.cs
@@ -4,6 +4,7 @@ using Liamell_Cruz_p1_Ap1.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Liamell_Cruz_p1_Ap1.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20250218005100_decimal")]
+    partial class @decimal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Liamell_Cruz_p1_Ap1/Migrations/20250218005100_decimal.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Liamell_Cruz_p1_Ap1.Migrations
+{
+    /// <inheritdoc />
+    public partial class @decimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Monto",
+                table: "Aportes",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Monto",
+                table: "Aportes",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
